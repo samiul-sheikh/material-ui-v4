3f6fa12edf6eb21861f30b72242d94dea4c8bd48
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-11-27 23:52:07 +0600

    radiobutton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BasicCard from './components/BasicCard';
 import ExploreGrid from './components/ExploreGrid';
+import RadioButton from './components/RadioButton';
 
 function App() {
   return (
     <div>
       <ExploreGrid />
       <BasicCard />
+      <RadioButton />
     </div>
   );
 }
